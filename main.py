aecc1838bd8676e9558b494a34348f4ab5d3ccb6
Tou-sif <tousifhaq777@gmail.com>
2023-07-19 10:37:34 +0600

    restarted

---
+++ main.py
@@ -0,0 +1,22 @@
+# #name, age, student = 'Tousif', 23, True
+
+# #print(name)
+# #print(age)
+# #print(student)
+
+# Bob = Pat = Sam = Squid = 30
+
+# print(Bob)
+# print(Pat)
+# print(Sam)
+# print(Squid)
+
+name = "touSif"
+print(len(name))
+print(name.find("t"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+
